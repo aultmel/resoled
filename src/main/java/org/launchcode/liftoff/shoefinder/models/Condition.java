a44713dde13bda2f579898893e56104fa28f57d8
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-06 14:49:03 -0500

    Added Condition and Image models from Speck's branch.  Commented out code on Condition model due to error coming up.  Login working

--- src/main/java/org/launchcode/liftoff/shoefinder/models/Condition.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/Condition.java
@@ -1,4 +1,55 @@
 package org.launchcode.liftoff.shoefinder.models;
 
-public class Condition {
-}
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Table;
+//import org.springframework.data.annotation.Id;
+//
+//@Entity
+//@Table(name = "condition")
+//public class Condition {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//    /*
+//    Enum to set the level of condition for the shoes. When a condition class is created:
+//    Condition condition = new Condition();
+//    the condition can be set by:
+//    condition.setCondition(Condition.ShoeCondition.NEW);
+//    .NEW, .LIKE_NEW, etc
+//     */
+//    public enum ShoeCondition {
+//        NEW("New"),
+//        LIKE_NEW("Like New"),
+//        GOOD("Good"),
+//        FAIR("Fair"),
+//        POOR("Poor");
+//
+//
+//        private final String displayText;
+//
+//        ShoeCondition(String displayText) {
+//            this.displayText = displayText;
+//        }
+//    }
+//
+//    private ShoeCondition condition;
+//
+//    public Condition(ShoeCondition condition) {
+//        this.condition = condition;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//
+//    public ShoeCondition getCondition() {
+//        return condition;
+//    }
+//
+//    public void setCondition(ShoeCondition condition) {
+//        this.condition = condition;
+//    }
+//}
--- src/main/java/org/launchcode/liftoff/shoefinder/models/Image.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/Image.java
@@ -1,4 +1,15 @@
 package org.launchcode.liftoff.shoefinder.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "image")
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Lob
+    @Column(name = "data", nullable = false, columnDefinition = "LONGBLOB")
+    private byte[] data;
 }

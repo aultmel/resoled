8f96c7a77e01c57d7a30425401bb7f2cef2d8f78
Adam <adam.dyer028@gmail.com>
2023-07-11 23:30:24 -0500

    dynamic filter on search page

--- src/main/java/org/launchcode/liftoff/shoefinder/data/BrandRepository.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/data/BrandRepository.java
@@ -1,11 +1,21 @@
 package org.launchcode.liftoff.shoefinder.data;
 
 import org.launchcode.liftoff.shoefinder.models.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-public interface BrandRepository {
-    @Query(nativeQuery = true, value = "SELECT * FROM brand ORDER BY popularity DESC")
-    List<Brand> findPopularBrands();
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+//    @Query(nativeQuery = true, value = "SELECT name, COUNT(name) AS frequency\n" +
+//            "FROM brand\n" +
+//            "GROUP BY name\n" +
+//            "ORDER BY frequency DESC\n" +
+//            "LIMIT 5;")
+@Query("SELECT b.name, COUNT(b.name) AS frequency " +
+        "FROM Brand b " +
+        "GROUP BY b.name " +
+        "ORDER BY COUNT(b.name) DESC")
+    List<Object[]> findPopularBrands();
 }

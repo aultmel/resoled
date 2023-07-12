6445dafd76aee0d057252aa4c1ea4fb280e29966
Adam <adam.dyer028@gmail.com>
2023-07-11 23:30:24 -0500

    dynamic filter on search page

--- src/main/java/org/launchcode/liftoff/shoefinder/controllers/SearchController.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/controllers/SearchController.java
@@ -8,18 +8,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class SearchController {
 
+    @Autowired
     private BrandRepository brandRepository;
 
     @GetMapping("/search")
     public String search(Model model){
-        List<Brand> popularBrands = brandRepository.findPopularBrands();
-        List<Brand> topFiveBrands = popularBrands.subList(0, 4);
-        model.addAttribute("topBrands", topFiveBrands);
+        List<Object[]> popularBrandsData = brandRepository.findPopularBrands();
+        List<String> popularBrands = new ArrayList<>();
+        for (Object[] brand : popularBrandsData) {
+            String brandName = (String) brand[0];
+            popularBrands.add(brandName);
+        }
+        model.addAttribute("topBrands", popularBrands);
+        List<String> shoeSizes = new ArrayList<>();
+        shoeSizes.add("5");
+        shoeSizes.add("6");
+        shoeSizes.add("7");
+        shoeSizes.add("8");
+        shoeSizes.add("9");
+        shoeSizes.add("10");
+        shoeSizes.add("11");
+        shoeSizes.add("12");
+        shoeSizes.add("13");
+        model.addAttribute("shoeSizes", shoeSizes);
+
         return "search";
     }
 
--- src/main/java/org/launchcode/liftoff/shoefinder/models/Brand.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/Brand.java
@@ -12,8 +12,6 @@ public class Brand {
     @Id
     @GeneratedValue
     private Long id;
-    @Formula("(SELECT COUNT(*) FROM Shoe s WHERE s.brand_id = id)")
-    private int popularity;
     private String name;
     public enum brandName {
         ADIDAS("Adidas"), NIKE("Nike"), GUCCI("Gucci"), JIMMYCHOO("Jimmy Choo"), CALVINKLIEN("Calvin Klien"), PUMA("Puma"), CROC("Croc"), ARIAT("Ariat"), REDWING("Redwing"), TIMBERLAND("Timberland"), VERSACE("Versace"), NEWBALANCE("New Balance"), CONVERSE("Converse"), CHINESELAUNDRY("Chinese Laundry"), HEELYS("Heelys"), JANSPORT("Jansport"), MADDENGIRL("Madden Girl"), REEBOK("Reebok"), VANS("Vans");

fa9dddf3a40a0cb88bce32fed72345300ea7036b
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-13 17:29:51 -0500

    fixed a small link issue and added basic error handling for creating a message.

--- src/main/java/org/launchcode/liftoff/shoefinder/data/UserRepository.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/data/UserRepository.java
@@ -2,9 +2,10 @@ package org.launchcode.liftoff.shoefinder.data;
 
 import org.launchcode.liftoff.shoefinder.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     UserEntity findByUsername(String username);

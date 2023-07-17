168ab98a2c9ce915535f481ef1c5f21c2543ef14
Heather <>
2023-07-16 21:03:38 -0500

    profile page added

--- src/main/java/org/launchcode/liftoff/shoefinder/controllers/UserController.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/controllers/UserController.java
@@ -1,4 +1,28 @@
 package org.launchcode.liftoff.shoefinder.controllers;
 
+import org.apache.catalina.User;
+import org.launchcode.liftoff.shoefinder.data.UserRepository;
+import org.launchcode.liftoff.shoefinder.models.UserEntity;
+import org.launchcode.liftoff.shoefinder.security.SecurityUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+//    @Autowired
+//    private UserRepository userRepository;
+
+    @GetMapping("/profile")
+    public String showProfile (Model model) {
+        UserEntity userEntity = new UserEntity();
+//        String username = SecurityUtility.getSessionUser();
+//        UserEntity userEntity = userRepository.findByUsername(username);
+        model.addAttribute("userEntity", userEntity);
+        return ("profiles/profile");
+    }
+
 }

8df53075aa671b15c6e30fc0cc858794ff1079bc
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-18 09:42:23 -0500

    small change

---
+++ src/main/java/org/launchcode/liftoff/shoefinder/security/JwtAuthEntryPoint.java
@@ -0,0 +1,20 @@
+package org.launchcode.liftoff.shoefinder.security;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+//THIS IS ALL A TEST
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}

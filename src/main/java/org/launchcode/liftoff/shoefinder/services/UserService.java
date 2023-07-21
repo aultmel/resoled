36e5a43cec47e461beacdb5d2b5f06476d610a32
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-21 17:26:00 -0500

    got the bugs worked out of pagenation and the messages refactor

--- src/main/java/org/launchcode/liftoff/shoefinder/services/UserService.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/services/UserService.java
@@ -8,12 +8,6 @@ import org.launchcode.liftoff.shoefinder.models.UserEntity;
 import org.launchcode.liftoff.shoefinder.models.dto.RegisterDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.stereotype.Service;

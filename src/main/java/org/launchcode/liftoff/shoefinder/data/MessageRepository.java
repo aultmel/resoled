c85aac003686a032b440e8b647a73cce6980b78e
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-21 16:02:24 -0500

    almost got it refactored

--- src/main/java/org/launchcode/liftoff/shoefinder/data/MessageRepository.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/data/MessageRepository.java
@@ -1,11 +1,16 @@
 package org.launchcode.liftoff.shoefinder.data;
 
 import org.launchcode.liftoff.shoefinder.models.Message;
+import org.launchcode.liftoff.shoefinder.models.MessageChain;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 
 //For Both Paging/Sorting and CRUD we use JPA Repository
 public interface MessageRepository extends JpaRepository<Message, Long>{
 
+    void findAll(List<MessageChain> userEntityMessageChains, Pageable pageable);
 }

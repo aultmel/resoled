d31af1ccaa8a741b68eafa06cec8e0de58ace7f6
bethb <beth.burch12@gmail.com>
2023-07-23 13:43:18 -0500

    added Image property to model

--- src/main/java/org/launchcode/liftoff/shoefinder/models/ShoeListing.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/ShoeListing.java
@@ -2,6 +2,9 @@ package org.launchcode.liftoff.shoefinder.models;
 //comments added
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "List")
 public class ShoeListing {
@@ -12,33 +15,36 @@ public class ShoeListing {
     @ManyToOne
     private UserEntity userEntity;
 
-//    @OneToOne
-//    private Location location;
+    @OneToOne
+    private Location location;
     @ManyToOne
     private Brand brand;
     @ManyToOne
     private Size size;
     @ManyToOne
     private Style style;
-//    private Condition condition;
-////    private Integer zipcode;
+    @ManyToOne
+    private Condition condition;
+
+    @OneToMany(mappedBy = "listing")
+    private List<Image> images = new ArrayList<>();
+
 
     //Empty Constructor
     public ShoeListing() {
 
     }
-//
-//    // Constructor with parameters
-//    public ShoeListing(Integer id, Brand brand, Size size, Style style, Condition condition, Location location, UserEntity userEntity) {
-//        this.id = id;
-//        this.brand = brand;
-//        this.size = size;
-//        this.style = style;
-//        this.condition = condition;
-////        this.zipcode = zipcode;
-//        this.location = location;
-//        this.userEntity = userEntity;
-//    }
+
+    // Constructor with parameters
+    public ShoeListing(Long id, Brand brand, Size size, Style style, Condition condition, Location location, UserEntity userEntity) {
+        this.id = id;
+        this.brand = brand;
+        this.size = size;
+        this.style = style;
+        this.condition = condition;
+        this.location = location;
+        this.userEntity = userEntity;
+    }
 //
     //Getters and Setters Methods
     public Long getId() {
@@ -52,46 +58,46 @@ public class ShoeListing {
     public void setUserEntity(UserEntity userEntity) {
         this.userEntity = userEntity;
     }
-//
-//    public Location getLocation() {
-//        return location;
-//    }
-//
-//    public void setLocation(Location location) {
-//        this.location = location;
-//    }
-//
-//    public Brand getBrand() {
-//        return brand;
-//    }
-//
-//    public void setBrand(Brand brand) {
-//        this.brand = brand;
-//    }
-//
-//    public Size getSize() {
-//        return size;
-//    }
-//
-//    public void setSize(Size size) {
-//        this.size = size;
-//    }
-//
-//    public Style getStyle() {
-//        return style;
-//    }
-//
-//    public void setStyle(Style style) {
-//        this.style = style;
-//    }
-//
-//    public Condition getCondition() {
-//        return condition;
-//    }
-//
-//    public void setCondition(Condition condition) {
-//        this.condition = condition;
-//    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
 }
 
 

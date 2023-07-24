44aa63b7e1fd8a66a9ff772194b203000885ea46
Heather <>
2023-07-24 00:33:34 -0500

    adding files

---
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/dto/EditProfileDTO.java
@@ -0,0 +1,12 @@
+package org.launchcode.liftoff.shoefinder.models.dto;
+
+public class EditProfileDTO {
+
+    private String displayName;
+
+
+    public String getDisplayName(){return displayName;}
+
+    public void setDisplayName(String displayName){this.displayName=displayName;}
+
+}

1e50982b0410b729dfba8f7716625b9693e079d3
Heather <>
2023-07-23 19:21:06 -0500

    added template

--- src/main/java/org/launchcode/liftoff/shoefinder/models/dto/EditProfileDTO.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/dto/EditProfileDTO.java
@@ -2,5 +2,11 @@ package org.launchcode.liftoff.shoefinder.models.dto;
 
 public class EditProfileDTO {
 
+    private String displayName;
+
+
+    public String getUsername(){return displayName;}
+
+    public void setUsername(String displayName){this.displayName=displayName;}
 
 }

d1d3c5004ad127edb783cca214b463a9bb6d7477
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-28 14:07:23 -0500

    updated the user lookup for new conversation to use displayName instead of username

--- src/main/java/org/launchcode/liftoff/shoefinder/models/dto/CreateMessageDTO.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/models/dto/CreateMessageDTO.java
@@ -12,7 +12,7 @@ private UserEntity senderUserEntity;
 
 private UserEntity receiverUserEntity;
 
-private String receiverUsername;
+private String receiverDisplayName;
 
 private String messageSubject;
 
@@ -27,23 +27,16 @@ private String text;
     public UserEntity getSenderUserEntity() {
         return senderUserEntity;
     }
-
     public void setSenderUserEntity(UserEntity senderUserEntity) {
         this.senderUserEntity = senderUserEntity;
     }
 
-    public String getReceiverUsername() {
-        return receiverUsername;
-    }
-
-    public void setReceiverUsername(String receiverUsername) {
-        this.receiverUsername = receiverUsername;
-    }
+    public String getReceiverDisplayName() { return receiverDisplayName; }
+    public void setReceiverDisplayName(String receiverDisplayName) { this.receiverDisplayName = receiverDisplayName; }
 
     public String getMessageSubject() {
         return messageSubject;
     }
-
     public void setMessageSubject(String messageSubject) {
         this.messageSubject = messageSubject;
     }
@@ -51,7 +44,6 @@ private String text;
     public String getText() {
         return text;
     }
-
     public void setText(String text) {
         this.text = text;
     }
@@ -59,7 +51,6 @@ private String text;
     public UserEntity getReceiverUserEntity() {
         return receiverUserEntity;
     }
-
     public void setReceiverUserEntity(UserEntity receiverUserEntity) {
         this.receiverUserEntity = receiverUserEntity;
     }

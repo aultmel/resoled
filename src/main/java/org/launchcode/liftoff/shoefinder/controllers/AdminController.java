b4221b5a85d391701be017bc330ecd29d68d04c4
Heather <>
2023-07-30 12:49:04 -0500

    adding in admin

---
+++ src/main/java/org/launchcode/liftoff/shoefinder/controllers/AdminController.java
@@ -0,0 +1,26 @@
+package org.launchcode.liftoff.shoefinder.controllers;
+
+import org.launchcode.liftoff.shoefinder.data.UserRepository;
+import org.launchcode.liftoff.shoefinder.models.UserEntity;
+import org.launchcode.liftoff.shoefinder.security.SecurityUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+        @Autowired
+        private UserRepository userRepository;
+
+        @GetMapping("")
+        public String showAdmin (Model model) {
+            String username = SecurityUtility.getSessionUser();
+            UserEntity userEntity = userRepository.findByUsername(username);
+            model.addAttribute("userEntity", userEntity);
+            return "admin";
+        }
+}

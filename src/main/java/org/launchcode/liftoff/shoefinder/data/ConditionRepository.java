fc99189a4fe2b57a33b7d0e6bae3202aac3f3a6c
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-31 20:21:15 -0500

    added fix

--- src/main/java/org/launchcode/liftoff/shoefinder/data/ConditionRepository.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/data/ConditionRepository.java
@@ -1,9 +1,9 @@
-package org.launchcode.liftoff.shoefinder.data;
-
-import org.launchcode.liftoff.shoefinder.models.Condition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConditionRepository extends JpaRepository<Condition, Long> {
-
-}
-
+//package org.launchcode.liftoff.shoefinder.data;
+//
+//import org.launchcode.liftoff.shoefinder.models.Condition;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ConditionRepository extends JpaRepository<Condition, Long> {
+//
+//}
+//

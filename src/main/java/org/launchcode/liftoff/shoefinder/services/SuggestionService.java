c145ee283b258fb445677fe1512c1bfe287a91a6
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-08-07 09:13:23 -0500

    spacing adjustment

--- src/main/java/org/launchcode/liftoff/shoefinder/services/SuggestionService.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/services/SuggestionService.java
@@ -70,18 +70,14 @@ public class SuggestionService {
     public String capitalizeAllFirstLetters(String aString)
     {
             String lowerCaseString = aString.toLowerCase();
-
             char[] array = lowerCaseString.toCharArray();
             array[0] = Character.toUpperCase(array[0]);
-
             for (int i = 1; i < array.length; i++) {
                 if (Character.isWhitespace(array[i - 1])) {
                     array[i] = Character.toUpperCase(array[i]);
                 }
             }
-
             return new String(array);
-
     }
 
 

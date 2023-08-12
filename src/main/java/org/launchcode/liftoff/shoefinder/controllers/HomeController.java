f6709c8c4859894fbfc718fd5120a645f7ecd2da
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-08-12 07:27:51 -0500

    fixing

--- src/main/java/org/launchcode/liftoff/shoefinder/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/controllers/HomeController.java
@@ -3,7 +3,6 @@ package org.launchcode.liftoff.shoefinder.controllers;
 
 import org.launchcode.liftoff.shoefinder.data.UserRepository;
 import org.launchcode.liftoff.shoefinder.models.UserEntity;
-import org.launchcode.liftoff.shoefinder.models.dto.CreateMessageDTO;
 import org.launchcode.liftoff.shoefinder.security.SecurityUtility;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,14 +29,11 @@ public class HomeController {
         UserEntity userEntity = userRepository.findByUsernameIgnoreCase(username);
         model.addAttribute("userEntity", userEntity);
 
-
         return "home";
     }
 
     @GetMapping("/home")
     public String homeGetMapping(Model model) {
-
-
 
         // sample of how to bring in user from the spring security.
         String username = SecurityUtility.getSessionUser();
--- src/main/java/org/launchcode/liftoff/shoefinder/controllers/ImageController.java
+++ src/main/java/org/launchcode/liftoff/shoefinder/controllers/ImageController.java
@@ -1,7 +1,6 @@
 
 package org.launchcode.liftoff.shoefinder.controllers;
 
-import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
